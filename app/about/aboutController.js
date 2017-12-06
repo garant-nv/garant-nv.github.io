4080ce63b23518d51b988c7a708c3f8694a4bce9
Dmytro Kryienty <dkryienty@expertcollege.com>
2017-12-06 11:11:43 +0200

    Added small fixes

--- app/about/aboutController.js
+++ app/about/aboutController.js
@@ -1,34 +1,43 @@
 ﻿app.controller('aboutController', function ($scope) {
-    $scope.slides = [
-            {
-                image: '/images/common/cabinets/2500/2500-min.jpg',
-                
-                id: 1
-            },
-            {
-                image: '/images/common/cabinets/1300/1300-min.jpg',
-
-                id: 2
-            },
-            {
-                image: '/images/common/cabinets/3300/3300-min.jpg',
-
-                id: 3
-            },
-            {
-                image: '/images/common/kitchens/elite/2.jpg',
-
-                id: 4
-            },
-            {
-                image: '/images/common/kitchens/platinum/11.jpg',
-
-                id: 5
-            },
-    ];
+    $scope.slides = [];
     $scope.myInterval = 1500;
     $scope.noWrapSlides = false;
     $scope.active = 0;
-    
+    init();
     console.log('about');
+    function init() {
+        for (var i = 0; i < 5; i++) {
+            $scope.slides.push({
+                image: '/images/main/' + (i + 1) + '.jpg',
+                //text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+                id: i
+            });
+        }
+        //$scope.slides.push({
+        //    image: '/images/common/cabinets/2500/2500-min.jpg',
+        //    id: 1
+        //});
+
+        //$scope.slides.push({
+        //        image: '/images/common/cabinets/1300/1300-min.jpg',
+
+        //        id: 2
+        //    });
+        //$scope.slides.push({
+        //        image: '/images/common/cabinets/3300/3300-min.jpg',
+
+        //        id: 3
+        //    });
+        //$scope.slides.push({
+        //        image: '/images/common/kitchens/elite/2.jpg',
+
+        //        id: 4
+        //    });
+        //$scope.slides.push({
+        //        image: '/images/common/kitchens/platinum/11.jpg',
+
+        //        id: 5
+        //    });
+        
+    }
 });
--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,3 +1,17 @@
 ﻿app.controller('homeController', function ($scope,$translate) {
-    console.log("home");
+    $scope.slides = [];
+    $scope.myInterval = 1500;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    init();
+    console.log('HOME');
+    function init() {
+        for (var i = 0; i < 2; i++) {
+            $scope.slides.push({
+                image: '/images/main' + (i + 1) + '.jpg',
+                text: 'HOME_TXT' + (i + 1),
+                id: i
+            });
+        }
+    }
 });
